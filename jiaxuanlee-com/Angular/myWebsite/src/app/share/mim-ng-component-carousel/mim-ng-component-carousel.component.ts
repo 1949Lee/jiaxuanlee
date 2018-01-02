cb85ad70e46781c35bb640f5627fb70badc8b976
1949Lee <lijiaxuan0811@hotmail.com>
2018-01-02 22:12:44 +0800

    导航实现

--- jiaxuanlee-com/Angular/myWebsite/src/app/share/mim-ng-component-carousel/mim-ng-component-carousel.component.ts
+++ jiaxuanlee-com/Angular/myWebsite/src/app/share/mim-ng-component-carousel/mim-ng-component-carousel.component.ts
@@ -1,27 +1,58 @@
-import { Component, OnInit, Inject, Input,enableProdMode  } from '@angular/core';
+import { Component, OnInit, Inject, Input, enableProdMode } from '@angular/core';
 import { LoggerService } from '../../_services/logger.service';
 import { appConfig } from '../../app.config';
 import { AfterViewInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { findIndex, map } from 'lodash';
 
 @Component({
   selector: 'app-mim-ng-component-carousel',
   templateUrl: './mim-ng-component-carousel.component.html',
   styleUrls: ['./mim-ng-component-carousel.component.scss']
 })
-export class MimNgComponentCarouselComponent implements OnInit, AfterViewInit {
+export class MimNgComponentCarouselComponent implements OnInit {
   @Input('options') carouselOption: any;
   wrapperHeight: string;
-  slides:any;
+  currentIndex: number;//当前图片下标
+  lastIndex: number;//上一个图片下标
+  prevIndex: number;//向左切换的图片下标
+  nextIndex: number;//向右切换的图片下标
+
 
   constructor( @Inject(appConfig) private app, private logger: LoggerService) { }
 
   ngOnInit() {
     this.logger.log(this.carouselOption)();
+    this.currentIndex = this.carouselOption.slides.findIndex((slide) => {
+      return slide.active;
+    });
   }
-  ngAfterViewInit() {
 
+  next() {
+    this.changeSlide("+");
   }
 
+  prev() {
+    this.changeSlide("-");
+  }
 
+  changeSlide(directve: any) {
+    switch (directve) {
+      case "+":
+        this.currentIndex = (this.currentIndex + 1) % this.carouselOption.slides.length;
+        break;
+      case "-":
+        this.currentIndex = (this.currentIndex - 1) < 0 ? this.carouselOption.slides.length - 1 : this.currentIndex - 1;
+        this.logger.log(this.currentIndex)();
+        break;
+      default:
+        this.currentIndex = directve;
+        break;
+    }
+    const temp: any = this.carouselOption.slides.map((slide, i) => {
+      slide.active = i == this.currentIndex ? true : false;
+      return slide;
+    });
+    this.carouselOption.slides = temp;
+  }
 
 }
