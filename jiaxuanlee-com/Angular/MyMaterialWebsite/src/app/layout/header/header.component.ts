d4092a175578cbaeccfe4cd956c210bb89c9561c
1949Lee <lijiaxuan0811@hotmail.com>
2018-01-21 14:44:23 +0800

    .

--- jiaxuanlee-com/Angular/MyMaterialWebsite/src/app/layout/header/header.component.ts
+++ jiaxuanlee-com/Angular/MyMaterialWebsite/src/app/layout/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input,Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Router ,ActivationStart,ActivationEnd} from '@angular/router';
+import { LoggerService } from '../../services/logger.service';
 
 @Component({
   selector: 'app-header',
@@ -9,7 +11,24 @@ export class HeaderComponent implements OnInit {
   @Output() sideNavToggle: EventEmitter<boolean> = new EventEmitter();
 
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private logger:LoggerService
+  ) {
+    this.router.events.subscribe( (e:any) =>{
+      if(e instanceof ActivationStart){
+        this.logger.log(e.snapshot.routeConfig)();
+        if(e.snapshot.routeConfig.path == "index"){
+          this.logger.log('导航首页开始')();
+        }
+      }
+      else if(e instanceof ActivationEnd){
+        if(e.snapshot.routeConfig.path == "index"){
+          this.logger.log('导航首页结束')();
+        }
+      }
+    })
+  }
 
   ngOnInit() {
 
