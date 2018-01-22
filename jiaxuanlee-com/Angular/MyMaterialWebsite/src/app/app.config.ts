9ef0e0541630c286a9e7035c77f700a19659fa27
1949Lee <lijiaxuan0811@hotmail.com>
2018-01-22 15:19:38 +0800

    .

--- jiaxuanlee-com/Angular/MyMaterialWebsite/src/app/app.config.ts
+++ jiaxuanlee-com/Angular/MyMaterialWebsite/src/app/app.config.ts
@@ -2,95 +2,110 @@ import { InjectionToken } from "@angular/core";
 
 export const appConfig = new InjectionToken<string>(`appConfig`);
 
-export interface myAppConfig{
-    lang:string;
-    isDev:boolean;
-    serverURL:string;
-    carousel:{[key:string]:any};
-    layout:{[key:string]:any};
-    footer:{[key:string]:any};
-    header:{[key:string]:any};
+export interface myAppConfig {
+    lang: string;
+    isDev: boolean;
+    serverURL: string;
+    carousel: { [key: string]: any };
+    layout: { [key: string]: any };
+    footer: { [key: string]: any };
+    header: { [key: string]: any };
 }
 const $asssets = `./assets/`;
 const $asssetsImg = `./assets/img/`;
 const dict = {
-    cn:{
-        header:{
-            items:{
-                '1':"首页",
-                '11':"首页1",
-                '12':"首页2",
-                '2':"豆瓣电影",
-                '21':"最新电影",
-                '22':"电影排行",
-                '3':"举例说明",
+    cn: {
+        header: {
+            items: {
+                '1': "首页",
+                '11': "首页1",
+                '12': "首页2",
+                '2': "豆瓣电影",
+                '21': "最新电影",
+                '22': "电影排行",
+                '3': "举例说明",
             }
         }
     },
-    en:{
-        header:{
-            items:{
-                '1':"Home",
-                '11':"Home1",
-                '12':"Home2",
-                '2':"Movies",
-                '21':"Latest Movies",
-                '22':"Top Movies",
-                '3':"For Example",
+    en: {
+        header: {
+            items: {
+                '1': "Home",
+                '11': "Home1",
+                '12': "Home2",
+                '2': "Movies",
+                '21': "Latest Movies",
+                '22': "Top Movies",
+                '3': "For Example",
             }
         }
     }
 }
 
-export class AppConfig implements myAppConfig{
+export class AppConfig implements myAppConfig {
     lang = "cn";
     isDev = true;
     serverURL = ``;
     carousel = {
-        slides:[
-            {imgURL:`${$asssetsImg}carousel/slide1.jpg`,active:true},
-            {imgURL:`${$asssetsImg}carousel/slide2.jpg`,active:false},
-            {imgURL:`${$asssetsImg}carousel/slide3.jpg`,active:false}
+        slides: [
+            { imgURL: `${$asssetsImg}carousel/slide1.jpg`, active: true },
+            { imgURL: `${$asssetsImg}carousel/slide2.jpg`, active: false },
+            { imgURL: `${$asssetsImg}carousel/slide3.jpg`, active: false }
         ],
-        imgResolution:{
-            width:1920,
-            height:1080,
+        imgResolution: {
+            width: 1920,
+            height: 1080,
         }
     };
     layout = {
-        breakpoints:{
-            xs:`(max-width: 575.99px) and (orientation: portrait), (max-width: 575.99px) and (orientation: landscape)`,
-            sm:`(min-width: 576px) and (max-width: 767.99px) and (orientation: portrait), (min-width: 576px) and (max-width: 767.99px) and (orientation: landscape)`,
-            md:`(min-width: 768px) and (max-width: 991.99px) and (orientation: portrait), (min-width: 768px) and (max-width: 991.99px) and (orientation: landscape)`,
-            lg:`(min-width: 992px) and (max-width: 1199.99px) and (orientation: portrait), (min-width: 992px) and (max-width: 1199.99px) and (orientation: landscape)`,
-            xl:`(min-width: 1200px) and (orientation: portrait), (min-width: 1200px) and (orientation: landscape)`,
+        breakpoints: {
+            xs: `(max-width: 575.99px) and (orientation: portrait), (max-width: 575.99px) and (orientation: landscape)`,
+            sm: `(min-width: 576px) and (max-width: 767.99px) and (orientation: portrait), (min-width: 576px) and (max-width: 767.99px) and (orientation: landscape)`,
+            md: `(min-width: 768px) and (max-width: 991.99px) and (orientation: portrait), (min-width: 768px) and (max-width: 991.99px) and (orientation: landscape)`,
+            lg: `(min-width: 992px) and (max-width: 1199.99px) and (orientation: portrait), (min-width: 992px) and (max-width: 1199.99px) and (orientation: landscape)`,
+            xl: `(min-width: 1200px) and (orientation: portrait), (min-width: 1200px) and (orientation: landscape)`,
         }
     };
     footer = {
-        height:72
+        height: 72
     };
-    header = {
-        isDragable:false,
-        items:[
-            {id:'index',text:dict[this.lang].header.items['1'],subItems:[dict[this.lang].header.items['11'],dict[this.lang].header.items['12']]},
-            {id:'movies',text:dict[this.lang].header.items['2'],subItems:[dict[this.lang].header.items['21'],dict[this.lang].header.items['22']]},
-            {id:'form',text:dict[this.lang].header.items['3'],subItems:null},
-        ]
-    };
-    constructor(){
-
+    header: any;
+    constructor() {
+        this.header = this.resetheader();
     }
-    
-    changeLang(l:string){
-        this.lang = l;
-        this.header = {
-            isDragable:false,
-            items:[
-                {id:'index',text:dict[this.lang].header.items['1'],subItems:[dict[this.lang].header.items['11'],dict[this.lang].header.items['12']]},
-                {id:'movies',text:dict[this.lang].header.items['2'],subItems:[dict[this.lang].header.items['21'],dict[this.lang].header.items['22']]},
-                {id:'form',text:dict[this.lang].header.items['3'],subItems:null},
+
+    resetheader() {
+        return {
+            isDragable: false,
+            items: [
+                {
+                    id: 'index', text: dict[this.lang].header.items['1'],
+                    active:true,
+                    subItems: [
+                        { text: dict[this.lang].header.items['11'] },
+                        { text: dict[this.lang].header.items['12'] }
+                    ]
+                },
+                {
+                    id: 'movies', text: dict[this.lang].header.items['2'],
+                    active:false,
+                    subItems: [
+                        { text: dict[this.lang].header.items['21'] },
+                        { text: dict[this.lang].header.items['22'] }
+                    ]
+                },
+                {
+                    id: 'form', text: dict[this.lang].header.items['3'],
+                    active:false,
+                    subItems: null
+                },
             ]
         };
     }
-} 
+
+    changeLang(l: string) {
+        this.lang = l;
+        this.header = this.resetheader();
+    }
+}
 export let AppConfigJSON = new AppConfig();
--- jiaxuanlee-com/Angular/MyMaterialWebsite/src/app/layout/header/header.component.ts
+++ jiaxuanlee-com/Angular/MyMaterialWebsite/src/app/layout/header/header.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter, Inject } from '@angular/core';
-import { Router ,ActivationStart,ActivationEnd} from '@angular/router';
+import { Router, ActivationStart, ActivationEnd } from '@angular/router';
 import { LoggerService } from '../../services/logger.service';
 import { appConfig } from '../../app.config';
+import { LeeService } from '../../services/lee.service';
 
 @Component({
   selector: 'app-header',
@@ -10,35 +11,58 @@ import { appConfig } from '../../app.config';
 })
 export class HeaderComponent implements OnInit {
   @Output() sideNavToggle: EventEmitter<boolean> = new EventEmitter();
+  public viewport: { [key: string]: any };
 
 
   constructor(
     private router: Router,
-    private logger:LoggerService,
-    @Inject(appConfig) private app,
+    private logger: LoggerService,
+    @Inject(appConfig) public app,
+    private lee: LeeService
   ) {
     this.logger.log(this.app.header)();
-    this.router.events.subscribe( (e:any) =>{
-      if(e instanceof ActivationStart){
+    this.router.events.subscribe((e: any) => {
+      if (e instanceof ActivationStart) {
         this.logger.log(e.snapshot.routeConfig)();
-        if(e.snapshot.routeConfig.path == "index"){
+        if (e.snapshot.routeConfig.path == "index") {
           // this.logger.log('导航首页开始')();
         }
       }
-      else if(e instanceof ActivationEnd){
-        if(e.snapshot.routeConfig.path == "index"){
+      else if (e instanceof ActivationEnd) {
+        if (e.snapshot.routeConfig.path == "index") {
           // this.logger.log('导航首页结束')();
         }
       }
     })
+    this.viewport = this.lee.viewport.ViewportSize();
   }
 
   ngOnInit() {
-
+    this.lee.viewport.viewport$.subscribe((vp) => {
+      this.viewport = vp;
+    })
   }
 
   doSideNavToggle() {
     this.sideNavToggle.emit(true);
+  }
+
+  menuButtonClick(i){
+    this.app.header.items = this.app.header.items.map((item,index) =>{
+      if(index == i){
+        item.active = true;
+        item.focus = true;
+      }
+      else{
+        item.active = false;
+        item.focus = false;
+      }
+      return item;
+    })
+  }
+
+  submenuButtonClick(i){
+
   }
 
 }
