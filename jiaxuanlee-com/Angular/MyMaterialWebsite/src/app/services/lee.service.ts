e594463acaa9f5a0cf04a30b08bb0a2a798523a6
1949Lee <lijiaxuan0811@hotmail.com>
2018-01-30 14:00:23 +0800

    .

--- jiaxuanlee-com/Angular/MyMaterialWebsite/src/app/services/lee.service.ts
+++ jiaxuanlee-com/Angular/MyMaterialWebsite/src/app/services/lee.service.ts
@@ -76,26 +76,79 @@ class ContentScroll {
   }
 }
 
+interface Media {
+  type:string;
+  mediaQuery:string;
+  matches:boolean;
+}
+
 @Injectable()
 class ResponsiveUI {
+
+  private xs:any;
+  private sm:any;
+  private md:any;
+  private lg:any;
+  private xl:any;
+  private curr:Media;
+  private all:any;
 
   constructor(
     @Inject(appConfig) private app,
     private logger: LoggerService,
     private media: ViewPort) {
-      this.currentMedia();
+      this.initMedia();
   }
 
-  currentMedia(){
-    let xs = window.matchMedia(`(max-width: 575.99px) and (orientation: portrait), (max-width: 575.99px) and (orientation: landscape)`);
-    let sm = window.matchMedia(`(min-width: 576px) and (max-width: 767.99px) and (orientation: portrait), (min-width: 576px) and (max-width: 767.99px) and (orientation: landscape)`);
-    let md = window.matchMedia(`(min-width: 768px) and (max-width: 991.99px) and (orientation: portrait), (min-width: 768px) and (max-width: 991.99px) and (orientation: landscape)`);
-    let lg = window.matchMedia(`(min-width: 992px) and (max-width: 1199.99px) and (orientation: portrait), (min-width: 992px) and (max-width: 1199.99px) and (orientation: landscape)`);
-    let xl = window.matchMedia(`(min-width: 1200px) and (orientation: portrait), (min-width: 1200px) and (orientation: landscape)`);
-    console.log(xs, sm, md, lg, xl);
+  initMedia(){
+    this.xs = window.matchMedia(this.app.breakpoints.xs);
+    this.sm = window.matchMedia(this.app.breakpoints.sm);
+    this.md = window.matchMedia(this.app.breakpoints.md);
+    this.lg = window.matchMedia(this.app.breakpoints.lg);
+    this.xl = window.matchMedia(this.app.breakpoints.xl);
+    this.resetCurrent();
+    return this.curr;
   }
 
-  xsChange() {
+  resetCurrent(){
+    this.curr = {
+      type:'',
+      mediaQuery:'',
+      matches:false
+    };
+    if(this.xs.matches){
+      this.curr.type = 'xs';
+      this.curr.mediaQuery = this.app.breakpoints.xs;
+      this.curr.matches = true;
+    }
+    if(this.sm.matches){
+      this.curr.type = 'sm';
+      this.curr.mediaQuery = this.app.breakpoints.sm;
+      this.curr.matches = true;
+    }
+    if(this.md.matches){
+      this.curr.type = 'md';
+      this.curr.mediaQuery = this.app.breakpoints.md;
+      this.curr.matches = true;
+    }
+    if(this.lg.matches){
+      this.curr.type = 'lg';
+      this.curr.mediaQuery = this.app.breakpoints.lg;
+      this.curr.matches = true;
+    }
+    if(this.xl.matches){
+      this.curr.type = 'xl';
+      this.curr.mediaQuery = this.app.breakpoints.xl;
+      this.curr.matches = true;
+    }
+    return this.curr;
+  }
+  
+  CurrentMedia(){
+    return this.resetCurrent();
+  }
+
+  xsChange(){
 
   }
 }
