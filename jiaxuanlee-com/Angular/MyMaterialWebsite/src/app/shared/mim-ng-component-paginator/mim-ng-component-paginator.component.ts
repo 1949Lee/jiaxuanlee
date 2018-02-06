34210c77ee99ba0f0d068f326c5d6171d934028a
1949Lee <lijiaxuan0811@hotmail.com>
2018-02-06 13:56:38 +0800

    .

---
+++ jiaxuanlee-com/Angular/MyMaterialWebsite/src/app/shared/mim-ng-component-paginator/mim-ng-component-paginator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mim-ng-component-paginator',
+  templateUrl: './mim-ng-component-paginator.component.html',
+  styleUrls: ['./mim-ng-component-paginator.component.scss']
+})
+export class MimNgComponentPaginatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- jiaxuanlee-com/Angular/MyMaterialWebsite/src/app/shared/shared.module.ts
+++ jiaxuanlee-com/Angular/MyMaterialWebsite/src/app/shared/shared.module.ts
@@ -3,24 +3,31 @@ import { CommonModule } from '@angular/common';
 import { MyMaterialModuleModule } from './my-material-module/my-material-module.module';
 import { MimNgComponentCarouselComponent } from './mim-ng-component-carousel/mim-ng-component-carousel.component';
 import { PageNotFoundComponent } from './utils/page-not-found/page-not-found.component';
-import {RouterModule} from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { MimNgComponentTabComponent } from './mim-ng-component-tab/mim-ng-component-tab.component';
+import { MimNgComponentPaginatorComponent } from './mim-ng-component-paginator/mim-ng-component-paginator.component';
 
 @NgModule({
   imports: [
     CommonModule,
     MyMaterialModuleModule,
     RouterModule,
-    
+
   ],
-  exports:[
+  exports: [
     CommonModule,
     MyMaterialModuleModule,
     MimNgComponentCarouselComponent,
     PageNotFoundComponent,
     RouterModule,
     MimNgComponentTabComponent,
+    MimNgComponentPaginatorComponent,
   ],
-  declarations: [MimNgComponentCarouselComponent,PageNotFoundComponent, MimNgComponentTabComponent,]
+  declarations: [
+    MimNgComponentCarouselComponent,
+    PageNotFoundComponent,
+    MimNgComponentTabComponent,
+    MimNgComponentPaginatorComponent,
+  ]
 })
 export class SharedModule { }
