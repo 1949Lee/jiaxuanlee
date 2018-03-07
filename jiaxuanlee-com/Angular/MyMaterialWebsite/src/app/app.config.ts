e1987fd803f2e383cff0be3595ba581769d4d674
1949Lee <lijiaxuan0811@hotmail.com>
2018-03-07 14:41:04 +0800

    .

--- jiaxuanlee-com/Angular/MyMaterialWebsite/src/app/app.config.ts
+++ jiaxuanlee-com/Angular/MyMaterialWebsite/src/app/app.config.ts
@@ -182,7 +182,7 @@ export class AppConfig implements myAppConfig {
         asssetsImgTech: `${$asssets}img/tech`,
     }
     serverURL = ``;
-    musicURL = `http://39.106.17.117:1000`;
+    musicURL = `https://musicapi.jiaxuanlee.com`;
     carousel = {
         slides: [
             { imgURL: `${$asssetsImg}carousel/slide1.jpg`, active: true },
